c1239faabac1c13a955627e49a6ca967e82aa4de
JohnE2199 <138347578+JohnG219@users.noreply.github.com>
2024-01-09 16:35:42 +0800

    Update Footer.jsx

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import {FaGoogle, FaVideo, FaAddressBook} from 'react-icons/fa';
 const Footer = () => {
   return (
     <div className="footer1">
-      <div className="fText">JOHN ERICK GONZALES Project@2023</div>
+      <div className="fText">©2023 JohnGonzales Project</div>
     </div>
   );
 };
